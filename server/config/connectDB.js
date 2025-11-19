b15de6c80d2523c0daf82024359958f08933d751
OfficalGaurav <officalgaurav10@gmail.com>
2025-11-19 14:49:32 +0530

    deploys

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -4,7 +4,7 @@ async function connectDB(){
     try {
         await mongoose.connect(process.env.MONGODB_URI)
 
-        const connection = mongoose.connection
+        const connection = mongoose.connection 
 
         connection.on('connected',()=>{
             console.log("Connect to DB")
